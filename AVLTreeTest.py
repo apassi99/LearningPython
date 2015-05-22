39502e5ac0b3825432d27d0d510887606ee5d128
apassi99 <apassi@live.com>
2015-05-22 17:58:10 -0400

    added test method for contains

--- AVLTreeTest.py
+++ AVLTreeTest.py
@@ -42,14 +42,41 @@ class AVLTreeTest(unittest.TestCase):
         self.tree.remove(150)
         self.assertEqual(self.tree.size, 0, "Incorrect tree size " + str(self.tree.size))
 
+    # Method to test contains function of AVLTree
     def test_contains(self):
         self.assertFalse(self.tree.contains(100))
         self.assertFalse(self.tree.contains(50))
         self.assertFalse(self.tree.contains(-50))
 
         self.tree.insert(100)
+        self.assertTrue(self.tree.contains(100))
+
         self.tree.insert(150)
+        self.assertTrue(self.tree.contains(100))
+        self.assertTrue(self.tree.contains(150))
+
         self.tree.insert(50)
+        self.assertTrue(self.tree.contains(100))
+        self.assertTrue(self.tree.contains(150))
+        self.assertTrue(self.tree.contains(50))
+
+        self.tree.remove(100)
+        self.assertFalse(self.tree.contains(100))
+        self.assertTrue(self.tree.contains(150))
+        self.assertTrue(self.tree.contains(50))
+
+        self.tree.remove(150)
+        self.tree.remove(50)
+        self.assertFalse(self.tree.contains(150))
+        self.assertFalse(self.tree.contains(50))
+
+        for i in range(1, 100):
+            self.tree.insert(i)
+            self.assertTrue(self.tree.contains(i))
+
+        for i in range(1, 100):
+            self.tree.remove(i)
+            self.assertFalse(self.tree.contains(i))
 
 if __name__ == '__main__':
     unittest.main()
